55c306fe24aa7807c8c3ed7d8d2b5410abecbd7f
DANIEL-RAMIREZ-RESTREPO <104943979+DANIEL-RAMIREZ-RESTREPO@users.noreply.github.com>
2022-10-05 17:12:03 -0500

    Add files via upload

---
+++ src/gui/VentanaPrincipal.java
@@ -0,0 +1,45 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class VentanaPrincipal extends JFrame
+{
+    Container panel;
+    Tablero lienzo;
+    Timer reloj;
+
+    public VentanaPrincipal() 
+    {
+        super("Ejemplo de animacion");
+        setSize(400, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        panel = getContentPane();
+        panel.setLayout(new FlowLayout());
+
+        JButton btnIniciar = new JButton("Iniciar");
+        panel.add(btnIniciar);
+
+        JButton btnDetener = new JButton("Detener");
+        panel.add(btnDetener);
+
+        reloj = new Timer(20, actionEvent -> 
+        {
+            lienzo.mover();
+            lienzo.repaint();
+        });
+
+        lienzo = new Tablero(reloj);
+        panel.add(lienzo);
+
+        btnIniciar.addActionListener(actionEvent -> 
+        {
+            reloj.start();
+        });
+
+        btnDetener.addActionListener(actionEvent -> 
+        {
+            reloj.stop();
+        });
+    }
+}
